1416c30d31cd3918b4f31170d1225b41080e05d7
Ehtasham Munib <emunib@outlook.com>
2022-05-14 16:01:40 -0600

    add connect to MongoDB and inventory model

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 require('dotenv').config();
 const {PORT} = process.env;
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// connect to db
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log('Connected to database');
+    } catch (err) {
+        console.log('Could not connect to database', err);
+    }
+};
+
+connectDB();
 
 // middleware
 app.use(cors());
@@ -12,6 +25,6 @@ app.use(express.json());
 
 
 // start server
-app.listen(PORT, function () {
-    console.log(`the server is running on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}`);
 });
---
+++ models/inventory.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const {Schema} = require('mongoose');
+
+
+const inventorySchema = new Schema({
+    itemName: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+        validate: {
+            validator: Number.isInteger,
+            message: '{VALUE} is not an integer value'
+        }
+    }
+});
+
+module.exports = mongoose.model("Inventory", inventorySchema)
